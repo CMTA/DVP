b4d5377bf82c95498da24768dc5542fac7f22c70
mh-targens <matthias.haeusser@targens.de>
2022-10-25 21:09:48 +0200

    Remove obsolete tasks

--- src/solidity/hardhat.config.ts
+++ src/solidity/hardhat.config.ts
@@ -1,12 +1,8 @@
 import { task, HardhatUserConfig } from "hardhat/config";
 import { deployDVP, upgradeDVPtoV2 } from "./scripts/deploy_contracts";
-import { getInfo, dvp_deactivateOldPot, dvp_readDeliveryExecuted, dvp_readSettlementCanceled, dvp_cancelSettlement } from "./scripts/test_dvp_setup";
+import { dvp_deactivateOldPot, dvp_readDeliveryExecuted, dvp_readSettlementCanceled, dvp_cancelSettlement } from "./scripts/test_dvp_setup";
 import { dvp_executeDelivery, dvp_readDeliveryConfirmed, dvp_getVersion, dvp_owner, dvp_getFixFunction } from "./scripts/test_dvp_setup";
 import { dvp_getPotAddress, dvp_setPotAddress, dvp_checkDeliveryForPot, initializeDvp } from "./scripts/test_dvp_setup";
-import { erc20_balanceOfSender, erc20_balanceOfReceiver, erc20_readTransfer, erc20_mint, erc20_balanceOfDvP } from "./scripts/test_dvp_setup";
-import { erc20_transfer, erc20_name, erc20_allowance, erc20_getVersion, erc20_increaseAllowance } from "./scripts/test_dvp_setup";
-import { pot_getBaseURI, pot_ownerOf, pot_initiatePayment, pot_confirmPayment, pot_transfer, pot_getBusinessId, pot_getCurrency, pot_getAmount, pot_getSender, pot_getReceiver, pot_getMintTime, pot_getVersion, pot_getDealDetailNum2, pot_getDealDetailNum, pot_getDealDetailAddress, pot_owner, pot_getStatus, deactivatePot } from "./scripts/test_dvp_setup";
-import { issuePaymentToken } from "./scripts/test_dvp_setup";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-solpp";
@@ -16,10 +12,6 @@ import "@nomiclabs/hardhat-etherscan";
 
 
 const { types } = require("hardhat/config")
-
-task("get_info", "Gets addresses of all contracts", async (taskArgs, hre) => {
-  console.log(await getInfo())
-})
 
 //Task to manually deploy and upgrade DvP-Contract
 task("deploy_dvp_contract", "Deploys DVP contract")
@@ -91,160 +83,6 @@ task("dvp_readDeliveryExecuted", "Returns all DeliveryExecuted events in the las
 
 task("dvp_readSettlementCanceled", "Returns all SettlementCanceled events in the last 10 blocks", async (taskArgs, hre) => {
   await dvp_readSettlementCanceled()
-})
-
-//Tasks to call POT SC functions
-task("pot_issuePaymentToken", "Issues POT at DvP-SC-Address from sender to receiver")
-    .addParam("num", "Number of AT to settle via POT", 5, types.int)
-    .addParam("tokenid", "Token-Id of POT to mint", 1, types.string)
-    .addParam("to", "Address to mint the POT to", "0", types.string)
-    .setAction(async (taskArgs) => {
-  await issuePaymentToken(taskArgs.num, taskArgs.tokenid, taskArgs.to)
-})
-
-task("pot_initiatePayment", "Initiates Payment for POT as sender")
-    .addParam("tokenid", "Token-Id of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-  await pot_initiatePayment(taskArgs.tokenid)
-})
-
-task("pot_confirmPayment", "Confirms Payment for POT as sender")
-    .addParam("tokenid", "Token-Id of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-  await pot_confirmPayment(taskArgs.tokenid)
-})
-
-task("pot_transfer", "Transfers POT from sender to DvP")
-    .addParam("tokenid", "Token-Id of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-  await pot_transfer(taskArgs.tokenid)
-})
-
-task("pot_deactivatePot", "Deactivates POT")
-  .addParam("tokenid", "TokenId of POT to deactivate", 1, types.string)
-  .setAction(async ({tokenid}) => {
-  await deactivatePot(tokenid)
-})
-
-task("pot_getStatus", "Gets status of POT")
-    .addParam("tokenid", "TokenId of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-        await pot_getStatus(taskArgs.tokenid)
-})
-
-task("pot_getVersion", "Calls version function of POT SC", async (taskArgs, hre) => {
-  console.log(await pot_getVersion())
-})
-
-task("pot_getBaseURI", "Gets Base-URI POT SC", async (taskArgs, hre) => {
-  console.log(await pot_getBaseURI())
-})
-
-task("pot_getDealDetailNum2", "Gets DealDetailNum2 of POT")
-    .addParam("tokenid", "TokenId of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-        await pot_getDealDetailNum2(taskArgs.tokenid)
-})
-
-task("pot_getDealDetailNum", "Gets DealDetailNum of POT")
-    .addParam("tokenid", "TokenId of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-        await pot_getDealDetailNum(taskArgs.tokenid)
-})
-
-task("pot_getDealDetailAddress", "Gets DealDetailAddress of POT")
-    .addParam("tokenid", "TokenId of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-        await pot_getDealDetailAddress(taskArgs.tokenid)
-})
-
-task("pot_getMintTime", "Gets MintTime of POT")
-    .addParam("tokenid", "TokenId of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-        await pot_getMintTime(taskArgs.tokenid)
-})
-
-task("pot_getReceiver", "Gets Receiver of POT")
-    .addParam("tokenid", "TokenId of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-        await pot_getReceiver(taskArgs.tokenid)
-})
-
-task("pot_getSender", "Gets Sender of POT")
-    .addParam("tokenid", "TokenId of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-        await pot_getSender(taskArgs.tokenid)
-})
-
-task("pot_getAmount", "Gets Amount of POT")
-    .addParam("tokenid", "TokenId of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-        await pot_getAmount(taskArgs.tokenid)
-})
-
-task("pot_getCurrency", "Gets Currency of POT")
-    .addParam("tokenid", "TokenId of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-        await pot_getCurrency(taskArgs.tokenid)
-})
-
-task("pot_getBusinessId", "Gets Business-ID of POT")
-    .addParam("tokenid", "TokenId of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-        await pot_getBusinessId(taskArgs.tokenid)
-})
-
-task("pot_ownerOf", "returns current owner of POT")
-    .addParam("tokenid", "TokenId of POT", 1, types.string)
-    .setAction(async (taskArgs) => {
-        await pot_ownerOf(taskArgs.tokenid)
-})
-
-task("pot_owner", "Returns current owner of POT SC", async (taskArgs, hre) => {
-  await pot_owner()
-})
-
-//Tasks to call ERC20 SC functions
-task("erc20_name", "Calls name function of ERC20 SC", async (taskArgs, hre) => {
-  await erc20_name()
-})
-
-task("erc20_getVersion", "Calls version function of DvP SC", async (taskArgs, hre) => {
-  await erc20_getVersion()
-})
-
-task("erc20_increaseAllowance", "Increases the allowance of DvP for receiver AT by 5", async (taskArgs, hre) => {
-  await erc20_increaseAllowance()
-})
-
-task("erc20_allowance", "Shows allowance of DvP for receiver AT", async (taskArgs, hre) => {
-  await erc20_allowance()
-})
-
-task("erc20_balanceOfDvP", "Shows balance AT of DvP address", async (taskArgs, hre) => {
-  await erc20_balanceOfDvP()
-})
-
-task("erc20_balanceOfReceiver", "Shows balance AT of Receiver address", async (taskArgs, hre) => {
-  await erc20_balanceOfReceiver()
-})
-
-task("erc20_balanceOfSender", "Shows balance AT of Sender address", async (taskArgs, hre) => {
-  await erc20_balanceOfSender()
-})
-
-task("erc20_mint", "Mints 5 AT as receiver address", async (taskArgs, hre) => {
-  await erc20_mint()
-})
-
-task("erc20_transfer", "Transfer 2 Token to other address")
-    .addParam("toaddress", "Address to transfer 2 token to", "", types.string)
-    .setAction(async (taskArgs) => {
-        await erc20_transfer(taskArgs.toaddress)
-})
-
-task("erc20_readTransfer", "Returns all transfer events in the last 10 blocks", async (taskArgs, hre) => {
-  await erc20_readTransfer()
 })
 
 const config: HardhatUserConfig = {
--- src/solidity/scripts/test_dvp_setup.ts
+++ src/solidity/scripts/test_dvp_setup.ts
@@ -8,41 +8,23 @@ const path = require("path");
 
 //define variables
 const senderAddress: string = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
-const receiverAddress: string = "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0"
-const potTokenId: BigNumber = BigNumber.from("25508521003322753386496418337090822004")
 
 //define provider
 const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545")
 const signerSender = provider.getSigner(senderAddress)
-const signerReceiver = provider.getSigner(receiverAddress)
 
-let potContract: Contract
 let dvpContract: Contract
-let erc20Contract: Contract
 
 let addresses
 let potAddress
 let dvpAddress
-let erc20Address
 
 async function main() {
     addresses = await getAddresses()
     if (addresses) {
         potAddress = addresses.pot
         dvpAddress = addresses.dvp
-        erc20Address = addresses.erc20
     }
-}
-
-export async function getInfo() {
-    let potVersion = await pot_getVersion()
-    let dvpVersion = await dvp_getVersion()
-    return {pot: potAddress,
-            potVersion: potVersion,
-            dvp: dvpAddress,
-            dvpVersion: dvpVersion,
-            potUsedByDvP: await createDvP().getPotAddress(),
-            erc20: erc20Address}
 }
 
 /**
@@ -74,25 +56,9 @@ function createDvP() {
 // creates DvPv2 once
 function createDvPv2() {
     if (dvpContract == null) {
-        dvpContract = createContract("DVPv2.sol/DVPv2.json", dvpAddress)
+        dvpContract = createContract("test/DVPv2.sol/DVPv2.json", dvpAddress)
     }
     return dvpContract
-}
-
-// creates POT once
-function createPOT() {
-    if (potContract == null) {
-        potContract = createContract("POT/POT.sol/POT.json", potAddress)
-    }
-    return potContract
-}
-
-// creates ERC20 once
-function createERC20() {
-    if (erc20Contract == null) {
-        erc20Contract = createContract("ERC20/ERC20_plain.sol/AssetToken.json", erc20Address)
-    }
-    return erc20Contract
 }
 
 //
@@ -172,183 +138,5 @@ export async function dvp_readSettlementCanceled() {
     let settlementCanceled = await createDvP().queryFilter(eventSettlementCanceled, -10)
     console.log(settlementCanceled)
 }
-
-
-//
-//POT functions
-//
-
-//mints POT
-export async function issuePaymentToken(amount: number, tokenId: number, to: string) {
-    let pot = await createPOT().issuePaymentToken(to, tokenId, "TestID", amount, 0, erc20Address, "CHF", 100, senderAddress, receiverAddress)
-    console.log(pot)
-}
-
-//calls initiatePayment for POT as sender
-export async function pot_initiatePayment(tokenId: number) {
-    let pot = await createPOT().initiatePayment(tokenId)
-    console.log(pot)
-}
-
-//calls confirmPayment for POT as sender
-export async function pot_confirmPayment(tokenId: number) {
-    let pot = await createPOT().confirmPayment(tokenId)
-    console.log(pot)
-}
-
-//transfers POT to DvP SC
-export async function pot_transfer(tokenId: number) {
-    let pot = await createPOT()['safeTransferFrom(address,address,uint256)'](senderAddress,dvpAddress,tokenId)
-    console.log(pot)
-}
-
-//deactivates POT at DvP address
-export async function deactivatePot(tokenId: BigNumber) {
-    await createPOT().deactivatePot(tokenId)
-}
-
-//calls getVersion
-export async function pot_getVersion() {
-    return await createPOT().getVersion()
-}
-
-//calls getBaseURI
-export async function pot_getBaseURI() {
-    return await createPOT().getBaseURI()
-}
-
-//calls getDealDetailNum
-export async function pot_getDealDetailNum(tokenId: BigNumber) {
-    let dealDetailNum = await createPOT().getDealDetailNum(tokenId)
-    console.log(dealDetailNum)
-}
-
-//calls getDealDetailNum2
-export async function pot_getDealDetailNum2(tokenId: BigNumber) {
-    let dealDetailNum2 = await createPOT().getDealDetailNum2(tokenId)
-    console.log(dealDetailNum2)
-}
-
-//calls getDealDetailAddress
-export async function pot_getDealDetailAddress(tokenId: BigNumber) {
-    let dealDetailAddress = await createPOT().getDealDetailAddress(tokenId)
-    console.log(dealDetailAddress)
-}
-
-//calls owner function of POT
-export async function pot_owner() {
-    let owner = await createPOT().owner()
-    console.log(owner)
-}
-
-//calls getStatus
-export async function pot_getStatus(tokenId: BigNumber) {
-    let status = await createPOT().getStatus(tokenId)
-    console.log(status)
-}
-
-//calls getMintTime
-export async function pot_getMintTime(tokenId: BigNumber) {
-    let mintTime = await createPOT().getMintTime(tokenId)
-    console.log(mintTime)
-}
-
-//calls getReceiver
-export async function pot_getReceiver(tokenId: BigNumber) {
-    let receiver = await createPOT().getReceiver(tokenId)
-    console.log(receiver)
-}
-
-//calls getSender
-export async function pot_getSender(tokenId: BigNumber) {
-    let sender = await createPOT().getSender(tokenId)
-    console.log(sender)
-}
-
-//calls getAmount
-export async function pot_getAmount(tokenId: BigNumber) {
-    let amount = await createPOT().getAmount(tokenId)
-    console.log(amount)
-}
-
-//calls getCurrency
-export async function pot_getCurrency(tokenId: BigNumber) {
-    let currency = await createPOT().getCurrency(tokenId)
-    console.log(currency)
-}
-
-//calls getBusinessId
-export async function pot_getBusinessId(tokenId: BigNumber) {
-    let businessId = await createPOT().getBusinessId(tokenId)
-    console.log(businessId)
-}
-
-//calls getBusinessId
-export async function pot_ownerOf(tokenId: BigNumber) {
-    let owner = await createPOT().ownerOf(tokenId)
-    console.log(owner)
-}
-
-//
-//ERC20 functions
-//
-
-//calls name function of ERC20
-export async function erc20_name() {
-    let name = await createERC20().name()
-    console.log(name)
-}
-
-//calls getVersion
-export async function erc20_getVersion() {
-    let version = await createERC20().getVersion()
-    console.log(version)
-}
-
-//calls balanceOf function of ERC20 for DvP
-export async function erc20_balanceOfDvP() {
-    let balanceBigNum = await createERC20().balanceOf(dvpAddress)
-    console.log(balanceBigNum.toNumber())
-}
-
-//calls balanceOf function of ERC20 for Receiver
-export async function erc20_balanceOfReceiver() {
-    let balanceBigNum = await createERC20().balanceOf(receiverAddress)
-    console.log(balanceBigNum.toNumber())
-}
-
-//calls balanceOf function of ERC20 for Sender
-export async function erc20_balanceOfSender() {
-    let balanceBigNum = await createERC20().balanceOf(senderAddress)
-    console.log(balanceBigNum.toNumber())
-}
-
-//calls mint function
-export async function erc20_mint() {
-    await createERC20().mint(receiverAddress,5)
-}
-
-//calls increaseAllowance
-export async function erc20_increaseAllowance() {
-    await createERC20().connect(signerReceiver).increaseAllowance(dvpAddress, 5)
-}
-
-//calls allowance
-export async function erc20_allowance() {
-    let allowanceBigNum = await createERC20().allowance(receiverAddress, dvpAddress)
-    console.log(allowanceBigNum.toNumber())
-}
-
-export async function erc20_transfer(toaddress: String) {
-    await createERC20().transfer(toaddress, 2)
-}
-
-//reads and returns Transfer-Events of ERC20
-export async function erc20_readTransfer() {
-    let transferFilter = createERC20().filters.Transfer()
-    let transfers = await createERC20().queryFilter(transferFilter, -10)
-    console.log(transfers)
-}
-
 
 main()
