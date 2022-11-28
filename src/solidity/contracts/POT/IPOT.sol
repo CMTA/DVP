4ec2511185d9cca2be58310e072291f58e7a505d
mh-targens <matthias.haeusser@targens.de>
2022-11-28 15:53:26 +0100

    CVF 22: import IERC721.sol instead of ERC721Pausable.sol

--- src/solidity/contracts/POT/IPOT.sol
+++ src/solidity/contracts/POT/IPOT.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.12 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 interface IPOT is IERC721
 {
