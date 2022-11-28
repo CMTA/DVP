e58c05dffcc25ff5961d52bc661eaf8308da915c
mh-targens <matthias.haeusser@targens.de>
2022-11-28 16:12:30 +0100

    CVF 21: Use "pragma solidity ^0.8.12;"

--- src/solidity/contracts/POT/IPOT.sol
+++ src/solidity/contracts/POT/IPOT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.12 <0.9.0;
+pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
--- src/solidity/contracts/POT/POT.sol
+++ src/solidity/contracts/POT/POT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.15 <0.9.0;
+pragma solidity ^0.8.12;
 
 // #def LOG false
 
